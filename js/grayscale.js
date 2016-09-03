699c364ad698bcf4cd7e2bd95a783f0ad6042398
j4v <xavier.garceau-aranda@polymtl.ca>
2016-09-03 14:28:44 +0200

    Update map & marker to Plaza del Sol

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.6700, -73.9400));
+    map.setCenter(new google.maps.LatLng(40.4168, -3.7035));
 });
 
 function init() {
@@ -45,10 +45,10 @@ function init() {
     // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
-        zoom: 15,
+        zoom: 18,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(40.6700, -73.9400), // New York
+        center: new google.maps.LatLng(40.4168, -3.7035), // New York
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.6700, -73.9400);
+    var myLatLng = new google.maps.LatLng(40.4168, -3.7035);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
