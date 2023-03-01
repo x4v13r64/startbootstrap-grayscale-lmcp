261385da6e98819af49c5632a29733035fe93704
x64-latacora <xavier@latacora.com>
2023-03-01 11:41:25 +0100

    Set correct location

--- js/grayscale.js
+++ js/grayscale.js
@@ -37,7 +37,7 @@ var map = null;
 // When the window has finished loading create our google map below
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
-    map.setCenter(new google.maps.LatLng(40.4168, -3.7035));
+    map.setCenter(new google.maps.LatLng(40.443982546746284, -3.700405991803525));
 });
 
 function init() {
@@ -176,7 +176,7 @@ function init() {
 
     // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/map-marker.png';
-    var myLatLng = new google.maps.LatLng(40.4168, -3.7035);
+    var myLatLng = new google.maps.LatLng(40.443982546746284, -3.700405991803525);
     var beachMarker = new google.maps.Marker({
         position: myLatLng,
         map: map,
